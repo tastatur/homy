841529a33dccaa5ccc71632a8c338cca9ecd8220
strah <strah@donttell>
2020-08-09 11:25:11 +0200

    Initial tempereture sensing

--- iot/arduino-mega/homy-arduino-mega/src/main.cpp
+++ iot/arduino-mega/homy-arduino-mega/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 52
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  dht.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+   if (isnan(humidity) || isnan(temperature)) {
+     Serial.println("DHT Sensor error");
+     return;
+   }
+
+   Serial.print("Temperature:");
+   Serial.println(temperature);
+
+   Serial.print("Humidity:");
+   Serial.println(humidity);
 }
